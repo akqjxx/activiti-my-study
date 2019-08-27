7e3bbea37d2fdd1ccb2435caad28e2c8ab6190ab
akqjxx <liuer2595919@163.com>
2019-08-27 18:24:10 +0800

    --

--- src/main/java/com/liujcc/activiti/controller/ProcessDefinitionsController.java
+++ src/main/java/com/liujcc/activiti/controller/ProcessDefinitionsController.java
@@ -2,19 +2,14 @@ package com.liujcc.activiti.controller;
 
 import com.google.common.collect.Maps;
 import com.liujcc.activiti.conf.SecurityUtil;
-import org.activiti.api.process.model.BPMNActivity;
 import org.activiti.api.process.model.ProcessDefinition;
 import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.api.process.model.ProcessInstanceMeta;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
-import org.activiti.api.task.model.builders.TaskPayloadBuilder;
-import org.activiti.api.task.model.payloads.GetTasksPayload;
 import org.activiti.api.task.runtime.TaskRuntime;
-import org.activiti.bpmn.model.BaseElement;
 import org.activiti.bpmn.model.BpmnModel;
-import org.activiti.bpmn.model.FlowElement;
 import org.activiti.bpmn.model.GraphicInfo;
 import org.activiti.bpmn.model.Process;
 import org.activiti.bpmn.model.SequenceFlow;
@@ -23,17 +18,13 @@ import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
-import org.activiti.engine.history.HistoricActivityInstance;
-import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -70,6 +61,7 @@ public class ProcessDefinitionsController {
 
     @GetMapping("/process-definitions")
     public List<ProcessDefinition> getProcessDefinitions() {
+        //获取系统流程定义
         Page<ProcessDefinition> processDefinitionPage = processRuntime.processDefinitions(Pageable.of(0, 10));
         logger.info("> Available Process definitions: " + processDefinitionPage.getTotalItems());
 
@@ -89,13 +81,16 @@ public class ProcessDefinitionsController {
         Task task = taskService.createTaskQuery()
                 .processInstanceId(processInstanceId)
                 .singleResult();
+        //获取流程定义
         ProcessDefinition processDefinition = processRuntime.processDefinition(task.getProcessDefinitionId());
         BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinition.getId());
 
+        //可以获取当前任务坐标信息
         GraphicInfo graphicInfo = bpmnModel.getGraphicInfo(task.getTaskDefinitionKey());
         Process process = bpmnModel.getProcesses().get(0);
-        UserTask flowElement = (UserTask)process.getFlowElement(task.getTaskDefinitionKey());
 
+        UserTask flowElement = (UserTask)process.getFlowElement(task.getTaskDefinitionKey());
+        //获取当前任务图连线 outgoing ,incoming
         List<SequenceFlow> outgoingFlows = flowElement.getOutgoingFlows();
         outgoingFlows.stream().forEach(flows->{
             System.out.println(flows.getName());
